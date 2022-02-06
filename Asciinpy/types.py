2313cb4ec50710ed2e74c0df64af21f9bde15f5f
Ricky <psmtitant@gmail.com>
2022-02-06 17:45:46 +0630

    addon types

--- Asciinpy/types.py
+++ Asciinpy/types.py
@@ -3,6 +3,10 @@ A collection of special types that are shared throughout
 the library - those private are locally defined.
 """
 
-from typing import Union
+from typing import List, TypeVar, Union, Tuple
 
 AnyInt = Union[int, float]
+T = TypeVar("T")
+Coordinate = Union[List[T], Tuple[T, T]]
+IntCoordinate = Coordinate[int]
+AnyIntCoordinate = Coordinate[AnyInt]
